7c066faeb2b11d0a745e7cc56bd2ad9543f83bda
Intuiti0n <danielgomescames@gmail.com>
2018-01-03 15:08:06 +0000

    changed all the code directories, added stuff for the main project, added support for the avr export project

---
+++ code/avr/avr_code/avr_code/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 03/01/2018 14:32:03
+ * Author : Daniel
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
